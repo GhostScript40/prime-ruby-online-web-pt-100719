04e21aa588369c397a1a8b56c47ec7dc33324da3
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-25 00:37:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,17 @@
 def prime?(num)
   n = 2
+<<<<<<< HEAD
     while n < num
       return false if num % n == 0
       n += 1
     end
 return false if num < 2
 n += 1
+=======
+  while n < num
+    return false if num < 0 
+    n += 1
+  end
+>>>>>>> ad58916bf5f3e59188e3432ab3a13c4ab2cba030
   true
 end
